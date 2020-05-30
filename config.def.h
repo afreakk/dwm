805b002d411ab38d1951faeffbdd4f663a4c512a
hans <laderud@hotmail.com>
2020-05-30 17:15:56 +0200

    remove bartabgroups

--- config.def.h
+++ config.def.h
@@ -17,9 +17,7 @@ static const char accent[]     = "#fabd2f";
 static const char *colors[][3] = {
 	/*               fg         bg         border   */
 	[SchemeNorm]        = { fg0,    bg0, bg0    },
-	[SchemeSel]         = { accent, bg2, accent },
-	[SchemeTabActive]   = { fg0,    bg0, accent },
-	[SchemeTabInactive] = { fg0,    bg0, accent }
+	[SchemeSel]         = { accent, bg0, accent },
 };
 
 /* tagging */
@@ -32,22 +30,12 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	/* { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 }, */
 };
 
 /* layout(s) */
 static const float mfact     = 0.61803398875; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-
-/* Bartabgroups properties */
-#define BARTAB_BORDERS 1       // 0 = off, 1 = on
-#define BARTAB_BOTTOMBORDER 1  // 0 = off, 1 = on
-#define BARTAB_TAGSINDICATOR 1 // 0 = off, 1 = on if >1 client/view tag, 2 = always on
-#define BARTAB_TAGSPX 5        // # pixels for tag grid boxes
-#define BARTAB_TAGSROWS 3      // # rows in tag grid (9 tags, e.g. 3x3)
-static void (*bartabmonfns[])(Monitor *) = { monocle /* , customlayoutfn */ };
-static void (*bartabfloatfns[])(Monitor *) = { NULL /* , customlayoutfn */ };
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
